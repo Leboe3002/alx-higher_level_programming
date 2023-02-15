be741d56f34a4d56ea7e09cd45e429d50fd24066
leboe3002 <og.leboe@gmail.com>
2023-02-15 10:49:54 -0800

    coding

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,4 +1,5 @@
-ecords are ordered by ascending show title.
+-- Lists all shows without the comedy genre in the database hbtn_0d_tvshows.
+-- Records are ordered by ascending show title.
 SELECT DISTINCT `title`
   FROM `tv_shows` AS t
        LEFT JOIN `tv_show_genres` AS s
